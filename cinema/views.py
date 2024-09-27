c1b7f7b9efb820f2f4bab36e114bc9015b066c79
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-09-27 22:43:57 +0300

    fixed patch

--- cinema/views.py
+++ cinema/views.py
@@ -87,6 +87,9 @@ class ActorDetail(
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
 
+    def patch(self, request, *args, **kwargs):
+        return self.update(request, partial=True, *args, **kwargs)
+
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
 
