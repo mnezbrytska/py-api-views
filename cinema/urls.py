85b96d6ac8d73d8f9aaf80837b20b51ab35b9257
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-09-28 10:30:08 +0300

    "fixed urls"

--- cinema/urls.py
+++ cinema/urls.py
@@ -14,6 +14,7 @@ app_name = "cinema"
 router = routers.DefaultRouter()
 router.register("movies", MovieViewSet)
 
+
 cinema_hall_list = CinemaHallViewSet.as_view(
     actions={
         "get": "list",
@@ -33,8 +34,8 @@ urlpatterns = [
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("halls/", cinema_hall_list, name="cinema_hall-list"),
-    path("halls/<int:pk>/", cinema_hall_detail, name="cinema_hall-detail"),
+    path("cinema_halls/", cinema_hall_list, name="cinema_hall-list"),
+    path("cinema_halls/<int:pk>/", cinema_hall_detail, name="cinema_hall-detail"),
     path("", include(router.urls)),
 
 
