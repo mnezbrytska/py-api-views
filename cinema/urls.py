21d59eedd35852508ccb78c8a3c5887d986ce1d5
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-09-28 10:32:43 +0300

    "fixed flake8"

--- cinema/urls.py
+++ cinema/urls.py
@@ -35,7 +35,11 @@ urlpatterns = [
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
     path("cinema_halls/", cinema_hall_list, name="cinema_hall-list"),
-    path("cinema_halls/<int:pk>/", cinema_hall_detail, name="cinema_hall-detail"),
+    path(
+        "cinema_halls/<int:pk>/",
+        cinema_hall_detail,
+        name="cinema_hall-detail"
+    ),
     path("", include(router.urls)),
 
 
